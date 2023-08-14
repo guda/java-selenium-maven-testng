33a4dd5eb82e6c893c488f9c1b1fa6b19682fd61
amir.gudic <agudic@vaulthealth.com>
2023-08-14 14:41:27 +0200

    tweak

--- src/test/java/assertions/VerifyPageValidationErrors.java
+++ src/test/java/assertions/VerifyPageValidationErrors.java
@@ -1,13 +1,10 @@
 package assertions;
 
-import java.util.Base64;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.asserts.SoftAssert;
-import pages.BasePage;
 
 public class VerifyPageValidationErrors {
 
@@ -16,7 +13,7 @@ public class VerifyPageValidationErrors {
     Assert.assertEquals(elements.size() > 0, shouldBeDisplayed);
   }
 
-  public static void verifyIfButtonIsDisabled(WebDriver driver, By locator, Boolean shouldBeDisabled){
+  public static void verifyIfButtonIsDisabled(WebDriver driver, By locator, Boolean shouldBeDisabled) {
     Assert.assertEquals(driver.findElement(locator).isEnabled(), !shouldBeDisabled);
   }
 }
